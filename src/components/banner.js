a9013078ba7b40f5ed8b57c4e5f20e42e1a8278a
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-04-26 14:42:13 +0530

    adding banner

---
+++ src/components/banner.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Banner from "../images/background.png"; // your imported banner image
+import Logo from "../images/logo-Photoroom.png";
+const CustomHeader = () => {
+  return (
+    <div
+      className="relative bg-cover bg-center p-10 text-white"
+      style={{ backgroundImage: `url(${Banner})` }} // use imported Banner here
+    >
+      {/* Dark overlay to make text pop */}
+      <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+
+      {/* Content on top of background */}
+      <div className="relative flex flex-col md:flex-row items-center justify-between gap-6">
+        {/* Left Column */}
+        <div className="flex-1 flex justify-center">
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/5ec694eda6af3254350f52f7/1590931907710-BXNFKRMBG9WPDDWMFMZL/Jar+of+Joy_Logo_S-01+copy.png"
+            alt="Find the joy in fitness"
+            className="max-w-xs"
+          />
+        </div>
+
+        {/* Middle Column */}
+        <div className="flex-1 flex justify-center">
+          <h1>
+            <a
+              id="logo"
+              href="https://www.blogilates.com"
+              data-supplied-ml="false"
+            >
+              <img src={Logo} alt="Blogilates logo" className="max-w-[150px]" />
+            </a>
+          </h1>
+        </div>
+
+        {/* Right Column */}
+        <div className="flex-1 flex justify-center">
+          <p className="text-lg font-bold text-center">
+            {new Date()
+              .toLocaleDateString("en-US", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })
+              .replace(",", "\n")}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomHeader;
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
+import CustomHeader from "../components/banner";
 function HomePage() {
   return (
     <div>
       <Navbar />
+      <CustomHeader />
       <Outlet />
     </div>
   );
