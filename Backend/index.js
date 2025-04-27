5d8b7edbabf49b8dde10f4a49a450da453d1883a
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-04-27 08:30:23 +0530

    defining routes for auth

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routers/authRoutes");
 
 require("dotenv").config();
 
@@ -8,6 +9,9 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
